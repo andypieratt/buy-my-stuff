2754bfd603741c5493178d2574f51253402f0a55
Andy Pieratt <andypieratt@Andys-MacBook-Pro.local>
2022-06-13 16:41:54 -0600

    got all the seeds seeded if that makes any sense and got the beginnings of my category and index models working

--- models/index.js
+++ models/index.js
@@ -11,18 +11,14 @@ Product.belongsTo(Category, {
 
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: "product_id",
+  foreignKey: "category_id",
 });
 
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag, {
-  foreignKey: "product_tag",
-});
+Product.belongsToMany(Tag, { through: ProductTag });
 
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product, {
-  foreignKey: "product_tag",
-});
+Tag.belongsToMany(Product, { through: ProductTag });
 
 module.exports = {
   Product,
